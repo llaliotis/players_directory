8a7c87aa3da9e52c0c35f1d305179f21de6fe5ab
llaliotis <llaliotis@gmail.com>
2023-08-12 12:44:34 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+
 function App() {
+  const [showAddForm, setShowAddForm] = useState(false);
+
+  const handleAddPlayersClick = () => {
+    setShowAddForm(true);
+  };
+
+  const handleBackClick = () => {
+    setShowAddForm(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Basketball Players Directory</h1>
+        {showAddForm ? (
+          <button onClick={handleBackClick}>Back</button>
+        ) : (
+          <button onClick={handleAddPlayersClick}>Add Players</button>
+        )}
       </header>
+      <main>
+        {showAddForm ? (
+          <iframe
+            src="https://airtable.com/embed/appThwwv8stVXpfrq/shrODyeVL0HL6pqPb?backgroundColor=purple"
+            title="Add Players Form"
+            width="100%"
+            height="600"
+          />
+        ) : (
+          <div>
+            <iframe
+              src="https://airtable.com/embed/appThwwv8stVXpfrq/shrn2FCFyeL1R6BVK?backgroundColor=purple"
+              title="Basketball Players Table"
+              width="100%"
+              height="600"
+            />
+          </div>
+        )}
+      </main>
     </div>
   );
 }
